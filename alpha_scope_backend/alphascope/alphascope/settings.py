721dc933bfaafde18be5eeda90b2a1cc1687de8c
Sharndeep Kaur <sharnsaini625@gmail.com>
2026-06-02 16:21:38 +0530

    modified-alphascope/setting.py

--- alpha_scope_backend/alphascope/alphascope/settings.py
+++ alpha_scope_backend/alphascope/alphascope/settings.py
@@ -136,14 +136,26 @@ WSGI_APPLICATION = 'alphascope.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ALPHASCOPE_DB',
+#         'USER':'root',
+#         'PASSWORD':'admin@123',
+#         'HOST':'localhost',
+#         'PORT':'3306'
+#     }
+# }
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ALPHASCOPE_DB',
-        'USER':'root',
-        'PASSWORD':'admin@123',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
