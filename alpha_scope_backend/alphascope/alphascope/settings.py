6c44cd2c684630e2aba62039829038bc4bbd4d3e
Sharndeep Kaur <sharnsaini625@gmail.com>
2026-06-03 17:57:48 +0530

    setting smtp.py

--- alpha_scope_backend/alphascope/alphascope/settings.py
+++ alpha_scope_backend/alphascope/alphascope/settings.py
@@ -201,14 +201,14 @@ CELERY_WORKER_POOL = 'solo'
 
 
 # Email settings 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='alphascope.team@gmail.com'      # sender
-EMAIL_HOST_PASSWORD = 'mklkcjtvbhsxvovj'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER ='alphascope.team@gmail.com'      # sender
+# EMAIL_HOST_PASSWORD = 'mklkcjtvbhsxvovj'
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Email config (use Gmail or any SMTP)
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
