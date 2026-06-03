508ca67c3502e96fb9008b328cb6932095fb14d5
Sharndeep Kaur <sharnsaini625@gmail.com>
2026-06-03 17:17:23 +0530

    fix smtp

--- alpha_scope_backend/alphascope/alphascope/settings.py
+++ alpha_scope_backend/alphascope/alphascope/settings.py
@@ -211,16 +211,25 @@ EMAIL_HOST_PASSWORD = 'mklkcjtvbhsxvovj'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Email config (use Gmail or any SMTP)
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = "alphascope.team@gmail.com"
+# EMAIL_HOST_PASSWORD = "mklkcjtvbhsxvovj"   # Gmail App Password, not your login password
+# DEFAULT_FROM_EMAIL = "AlphaScope <alphascope.team@gmail.com>"
+
+
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "alphascope.team@gmail.com"
-EMAIL_HOST_PASSWORD = "mklkcjtvbhsxvovj"   # Gmail App Password, not your login password
-DEFAULT_FROM_EMAIL = "AlphaScope <alphascope.team@gmail.com>"
 
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 # Frontend URL used in reset link
-FRONTEND_URL = "http://localhost:5173"  # change to production URL when deploying
+FRONTEND_URL = "https://alpha-scope-neon.vercel.app"  # change to production URL when deploying
 from celery.schedules import crontab
 
 CELERY_BEAT_SCHEDULE = {
